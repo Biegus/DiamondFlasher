9477e0e1215c4fc3a3906f6035949dd070c90fdf
TheMatiaz0 <TheMatiaz0@protonmail.com>
2021-01-01 14:04:24 +0100

    Update Program.cs

--- DiamondFlasher/Program.cs
+++ DiamondFlasher/Program.cs
@@ -10,7 +10,7 @@ namespace DiamondFlasher
 
             WordsBank bank = new WordsBank();
             MainContext main = new MainContext(bank);
-            ConsoleHelper.WriteLine($"All started, type <{Styles.Command}>\"help\"</color> to see commands in current context", Styles.Information);
+            ConsoleHelper.WriteLine($"All started, type <{Styles.Command}>\"help\"</color> to see commands in the current context", Styles.Information);
             Context.LoadContext<MainContext>(main);
         }
 
